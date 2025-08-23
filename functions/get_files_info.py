ed1abb23c02419cb7b558a6a85bde1de7192ed10
gooneraki <hayiate4@hotmail.com>
2025-08-23 16:37:05 +0300

    Refactor main.py to integrate schema_get_files_info and enhance system prompt; improve argument handling and response processing

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,15 +1,5 @@
 import os
-
-# Here are some standard library functions you'll find helpful:
-
-# os.path.abspath(): Get an absolute path from a relative path
-# os.path.join(): Join two paths together safely (handles slashes)
-# .startswith(): Check if a string starts with a substring
-# os.path.isdir(): Check if a path is a directory
-# os.listdir(): List the contents of a directory
-# os.path.getsize(): Get the size of a file
-# os.path.isfile(): Check if a path is a file
-# .join(): Join a list of strings together with a separator
+from google.genai import types
 
 
 def get_files_info(working_directory, directory="."):
@@ -36,3 +26,18 @@ def get_files_info(working_directory, directory="."):
         return files_ext
     except Exception as e:
         return f"Error: {e}"
+    
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -1,40 +1,67 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai
+from google.genai import types, Client
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
     load_dotenv()
     args = sys.argv
 
-    if len(args)==1:
+    if len(args) == 1:
         print("Requires argument for PROMPT. usage `uv run main.py PROMPT [--verbose]`")
         sys.exit(1)
 
     user_prompt = args[1]
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     is_verbose = "--verbose" in args
+    model_name = "gemini-2.0-flash-001"
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    config = types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+
     if is_verbose:
         print(f"User prompt: {user_prompt}")
 
     messages = [
-        genai.types.Content(role="user", parts=[genai.types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-
 
     api_key = os.environ.get("GEMINI_API_KEY")
 
-    client = genai.Client(api_key=api_key)
+    client = Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model=model_name,
+        contents=messages,
+        config=config,
     )
 
-    print(response.text)
+    function_calls = response.function_calls
+    if function_calls:
+        for function_call_part in function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+    else:
+        print(response.text)
 
     if is_verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
 
 
 if __name__ == "__main__":
